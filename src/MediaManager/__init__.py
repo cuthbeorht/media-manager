947f2c8d721c977c04542c71ecccbbc1163bc285
David Sciacchettano <david.sciacchettano@gmail.com>
2026-09-07 21:02:39 -0400

    Fix formatting

--- src/MediaManager/__init__.py
+++ src/MediaManager/__init__.py
@@ -12,8 +12,7 @@ class MyWidget(QtWidgets.QWidget):
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
         self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
@@ -24,6 +23,7 @@ class MyWidget(QtWidgets.QWidget):
     @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
+
 
 def main() -> None:
     print("Hello from media-manager!")
